c79d6788639673366b62fef836c8ff93a90f58cf
ErykDarnowski <github@erykdarnowski.pl>
2022-10-24 17:48:31 +0200

    Implement not going over `.git` dirs and checking if path has been given

--- TODO.md
+++ TODO.md
@@ -3,8 +3,7 @@
 - [ ] Select all by yourself (lang / name)
 - [ ] Auto select the lang ones by detecting ext and than add additional ones yourself
   - loose the dot?
-  - get path as arg
-  - print ext that couldn't be identified
-  - don't go over `.git` folder
-  - blacklist certain extensions?
+  - sort ext alphabetical
   - count amount of each file?
+  - print ext that couldn't be identified
+  - blacklist certain extensions / folders (like `.git` / `.expo cache`)?
--- get_ext.py
+++ get_ext.py
@@ -6,13 +6,23 @@ import sys
 workdir = sys.argv[-1] # "./" # dir to work in
 extensions = set()
 
-# Get the distinct extensions:
-for root, dirs, files in os.walk(workdir):
-    for file in files:
-        pathname, exten = os.path.splitext(file)
-        if (exten != ""):
-            extensions.add(exten)
+# Check if any path has been given:
+if len(sys.argv) == 1:
+    print("You need to give path to dir!!!")
+    exit()
 
-# Print them:
+# Get distinct extensions:
+for root, dirs, files in os.walk(workdir):
+    if ".git" not in root:
+        for file in files:
+            pathname, exten = os.path.splitext(file)
+            if (exten != ""):
+                extensions.add(exten)
+            else:
+                extensions.add(pathname)
+
+# Print distinct extensions:
 for ext in extensions:
     print(ext)
+
+exit()
