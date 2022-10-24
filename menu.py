af0b0a08151176b6b62da75732924b6f7cf8973d
ErykDarnowski <github@erykdarnowski.pl>
2022-10-24 19:56:02 +0200

    Format fixes for menu.py

--- menu.py
+++ menu.py
@@ -7,7 +7,7 @@ from simple_term_menu import TerminalMenu
 options = [
     "Automatic (suggests hooks groups based on file extensions and let's you pick the rest)",
     "Manual (you manually pick every hook group)",
-    "exit"
+    "exit",
 ]
 terminal_menu = TerminalMenu(options)
 
@@ -16,13 +16,15 @@ def main():
 
     # CLEAR SCREEN (get func from a helper sort of file?)
 
-    print("""                       ____  _____ _     _____ ____ _____ ___  ____  
+    print(
+        """                       ____  _____ _     _____ ____ _____ ___  ____  
    _ __  _ __ ___     / ___|| ____| |   | ____/ ___|_   _/ _ \|  _ \ 
   | '_ \| '__/ _ \____\___ \|  _| | |   |  _|| |     | || | | | |_) |
   | |_) | | |  __/_____|__) | |___| |___| |__| |___  | || |_| |  _ < 
   | .__/|_|  \___|    |____/|_____|_____|_____\____| |_| \___/|_| \_\\
   |_|                                                                
-""")
+"""
+    )
 
     # https://github.com/IngoMeyer441/simple-term-menu # <- styling options and so on
     menu_entry_index = terminal_menu.show()
