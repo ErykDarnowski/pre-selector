36bfeac67c6386428183aae98edb68b0a2998012
ErykDarnowski <github@erykdarnowski.pl>
2022-10-26 21:27:40 +0200

    Formatting and add ASCII colors in `menu.py`

--- menu.py
+++ menu.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 # Imports
+import os
+from colorama import Fore
+from colorama import Style
 from simple_term_menu import TerminalMenu
 
 # Vars
@@ -12,22 +15,35 @@ options = [
 terminal_menu = TerminalMenu(options)
 
 # Funcs
+def clear():  # CLEAR SCREEN (get func from a helper sort of file?)
+    os.system("cls" if os.name == "nt" else "clear")
+
+
 def main():
-    # CLEAR SCREEN (get func from a helper sort of file?)
+    clear()
 
     print(
-        """                       ____  _____ _     _____ ____ _____ ___  ____  
-   _ __  _ __ ___     / ___|| ____| |   | ____/ ___|_   _/ _ \|  _ \ 
-  | '_ \| '__/ _ \____\___ \|  _| | |   |  _|| |     | || | | | |_) |
-  | |_) | | |  __/_____|__) | |___| |___| |__| |___  | || |_| |  _ < 
-  | .__/|_|  \___|    |____/|_____|_____|_____\____| |_| \___/|_| \_\\
-  |_|                                                                
+        f"""{Fore.YELLOW}                       {Fore.RED}____  _____ _     _____ ____ _____ ___  ____  
+{Fore.YELLOW}   _ __  _ __ ___     {Fore.RED}/ ___|| ____| |   | ____/ ___|_   _/ _ \|  _ \ 
+{Fore.YELLOW}  | '_ \| '__/ _ \____{Fore.RED}\___ \|  _| | |   |  _|| |     | || | | | |_) |
+{Fore.YELLOW}  | |_) | | |  __/_____{Fore.RED}|__) | |___| |___| |__| |___  | || |_| |  _ < 
+{Fore.YELLOW}  | .__/|_|  \___|    {Fore.RED}|____/|_____|_____|_____\____| |_| \___/|_| \_\\
+{Fore.YELLOW}  |_|
 """
     )
 
     # https://github.com/IngoMeyer441/simple-term-menu # <- styling options and so on
     menu_entry_index = terminal_menu.show()
-    print(f"You have selected {options[menu_entry_index]}!")
+
+    clear()
+
+    # print(f"You have selected {options[menu_entry_index]}!")
+    if menu_entry_index == 0:
+        print("Automatic")
+    elif menu_entry_index == 1:
+        print("Manual")
+    else:
+        exit()
 
 
 if __name__ == "__main__":
