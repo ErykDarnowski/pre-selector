9aaf9ace9705c70aaab4d98278580ac24fe2a7c2
ErykDarnowski <github@erykdarnowski.pl>
2022-10-28 19:11:41 +0200

    Remove old `get_ext` approach and rename current one

--- get_ext.py
+++ get_ext.py
@@ -3,43 +3,39 @@
 # Imports
 import os
 import sys
+import git
+import pathlib
+from prettytable import PrettyTable
 
 # Vars
-extensions = set()
-workdir = sys.argv[-1]  # dir to work in (scan)
-dir_blacklist = [".git", "DEV"]
-ext_blacklist = [".spl"]
-
+path = sys.argv[-1]  # dir to work in (scan)
+g = git.Git(path)
 
 # Check if any path has been given:
 if len(sys.argv) == 1:
     sys.exit("You need to give path to dir!!!")
 
-# Get distinct extensions:
-for root, dirs, files in os.walk(workdir):
-    # Not looking through blacklisted dirs:
-    print(root.replace(workdir, ""))
-    # if any(dir in root for dir in dir_blacklist):
-    if ".git" not in root:
-        for file in files:
-            pathname, exten = os.path.splitext(file)
 
-            # Checking if extension is missing (for files like: `.gitignore`) for example:
-            if exten != "":
-                final = exten  # .replace(".", "")
-            else:
-                final = pathname  # .replace(".", "")
+files = g.execute(["git", "ls-files"]).split("\n")
 
-            # Not adding blacklisted extensions:
-            if final not in ext_blacklist:
-                extensions.add(final)
-
-# Sort extensions alphabetical:
-extensions = sorted(extensions)
-
-# Print extensions:
+# GET THIS FROM HELPER FILE!
 os.system("cls" if os.name == "nt" else "clear")
-for ext in extensions:
-    print(ext)
 
-exit()
+table = PrettyTable(
+    ["Num", "Rel path", "Filename", "Ext (suffix)", "Ext (suffixes)", "Ext (splitext)"]
+)
+table.align = "l"
+for i in range(0, len(files)):
+    # print(f"{i + 1}. {files[i]} = {pathlib.Path(files[i]).suffix} / {pathlib.Path(files[i]).suffixes} / {os.path.splitext(files[i])[1]}") # <- joing the suffixes? / use suffix?
+    table.add_row(
+        [
+            i + 1,
+            files[i],
+            os.path.basename(files[i]),
+            pathlib.Path(files[i]).suffix,
+            pathlib.Path(files[i]).suffixes,
+            os.path.splitext(files[i])[1],
+        ]
+    )
+
+print(table)
