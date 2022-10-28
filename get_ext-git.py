8dda4c4bdfc0235cf3de8d80ad02843a339df5d4
ErykDarnowski <github@erykdarnowski.pl>
2022-10-28 14:38:47 +0200

    Add initial `get_ext-git.py`

---
+++ get_ext-git.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+# Imports
+import os
+import sys
+import git
+import pathlib
+from prettytable import PrettyTable
+
+# Vars
+path = sys.argv[-1]  # dir to work in (scan)
+g = git.Git(path)
+
+# Check if any path has been given:
+if len(sys.argv) == 1:
+    sys.exit("You need to give path to dir!!!")
+
+
+files = g.execute(["git", "ls-files"]).split('\n')
+
+# GET THIS FROM HELPER FILE!
+os.system("cls" if os.name == "nt" else "clear")
+
+
+"""
+for i in range(0, len(files)):
+    print(f"{i + 1}. {files[i]} = {pathlib.Path(files[i]).suffix} / {pathlib.Path(files[i]).suffixes} / {os.path.splitext(files[i])[1]}") # <- joing the suffixes? / use suffix?
+"""
+
+t = PrettyTable(['Num', 'Path', 'Ext (suffix)', 'Ext (suffixes)', 'Ext (splitext)'])
+t.align = "l"
+for i in range(0, len(files)):
+    t.add_row([i + 1, files[i], pathlib.Path(files[i]).suffix, pathlib.Path(files[i]).suffixes, os.path.splitext(files[i])[1]])
+print(t)
